38998987ee036cc0aaedfa10c25f9c38a79588ba
Neto <w.neto06@gmail.com>
2022-03-27 23:50:55 -0300

    Tela inicial

--- script.js
+++ script.js
@@ -2,21 +2,59 @@ var btn_add = document.getElementById('add');
 var btn_addTarefa = document.getElementById('addTarefa');
 var btn_excluirTarefa = document.getElementById('removerTarefa');
 var form = document.getElementById('tarefas');
-var box = documento.getElementById('box');
-
 
 btn_add.addEventListener('click', function(){
+    createCheckBox();
     createImput();
-})
+    createBtnAceitar();
+    createBtnRemover();
+    createBr();
+    
+});
 
-//<input type="text" name="tarefa" id="tarefa" autocomplete="off"/>
+
+//<input type="text" name="tarefa" id="tarefa" class="text" autocomplete="off"/>
 function createImput(){
-    var elemento = document.createElement('tarefa');
+    var elemento = document.createElement('input');
     elemento.setAttribute('type', 'text');
     elemento.setAttribute('name', 'tarefa2');
     elemento.setAttribute('id', 'tarefa2');
-    elemento.setAttribute('autocomplete','off');
+    elemento.setAttribute('autocomplete', 'off');
+    elemento.setAttribute('class', 'text');
     
-    box.appendChild(elemento);
+    form.appendChild(elemento);
+};
 
+//<input for="tarefa" class="marcarTarefa" type="checkbox"></input>
+function createCheckBox(){
+    var elemento = document.createElement('input');
+    elemento.setAttribute('type', 'checkbox');
+    elemento.setAttribute('for', 'tarefa2');
+    elemento.setAttribute('class', 'marcarTarefa');
+
+    form.appendChild(elemento);
+};
+
+// <button for="tarefa" class="aceitarTarefa">V</button>
+function createBtnAceitar(){
+    var elemento = document.createElement('button');
+    elemento.setAttribute('for', 'tarefa2');
+    elemento.setAttribute('class', 'aceitarTarefa');
+    elemento.textContent = 'V';
+
+    form.appendChild(elemento);
+}
+
+//<button for="tarefa" class="deletarTarefa">L</button>
+function createBtnRemover(){
+    var elemento = document.createElement('button');
+    elemento.setAttribute('for', 'tarefa2');
+    elemento.setAttribute('class', 'deletarTarefa');
+    elemento.textContent = 'L';
+    form.appendChild(elemento);
+}
+
+function createBr(){
+    var elemento = document.createElement('br');
+    form.appendChild(elemento);
 }
