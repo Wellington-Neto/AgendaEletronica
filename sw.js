c93ba50bbe197643bb24f1c7b82cede0998c34dd
Neto <w.neto06@gmail.com>
2022-04-02 20:05:44 -0300

    Comit Inicial

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
 self.addEventListener('install', function(event){
   console.log('sw1: instalado!', event)
 })
-
-self.addEventListener('activate', function(evente){
-  console.log('sw: ativado!', event)
-})
